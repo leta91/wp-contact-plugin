1980b1723758a0d4d0a33ca1e5ab6c4d0c5204aa
Letizia DI MAIO <109817389+leta91@users.noreply.github.com>
2023-06-28 04:47:59 +0100

    to protect plugin

---
+++ wp-contact-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: WP Contact Form Plugin
+ * Description: contact form plugin for WordPress 
+ * Author: Letizia DI MAIO
+ * Author URI: https://letiziadimaio.netlify.app/
+ * Version: 1.0.0
+ * Text Domain: wp-contact-plugin
+ */
